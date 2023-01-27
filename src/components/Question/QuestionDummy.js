49c6f2e35d1cc92aea1bc6e5f592b0e89a6a467d
ryukyeongseo <113505062+ryukyeongseo@users.noreply.github.com>
2023-01-27 14:12:03 +0900

    add test button page

--- src/components/Question/QuestionDummy.js
+++ src/components/Question/QuestionDummy.js
@@ -41,4 +41,60 @@ export const questionLists = [
             { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 'n' }
         ]
     },
+    {
+        id: '5',
+        question: 's/n질문3',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 's' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 'n' }
+        ]
+    },
+    {
+        id: '6',
+        question: 'f/t질문1',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'f' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 't' }
+        ]
+    },
+    {
+        id: '7',
+        question: 'f/t질문2',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'f' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 't' }
+        ]
+    },
+    {
+        id: '8',
+        question: 'f/t질문3',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'f' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 't' }
+        ]
+    },
+    {
+        id: '9',
+        question: 'p/j질문1',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'p' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 'j' }
+        ]
+    },
+    {
+        id: '10',
+        question: 'p/j질문2',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'p' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 'j' }
+        ]
+    },
+    {
+        id: '11',
+        question: 'p/j질문3',
+        selection: [
+            { answer: '새로운 사람들과 친해지려고 노력한다', value: 'p' },
+            { answer: '아는 사람과 깊게 친해지려고 노력한다', value: 'j' }
+        ]
+    },
 ]
