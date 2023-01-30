9fa76ce9d59bce1dbdf53f6ab66f4ba281591999
ryukyeongseo <113505062+ryukyeongseo@users.noreply.github.com>
2023-01-30 22:59:39 +0900

    add to button value

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -5,7 +5,8 @@ import React from "react";
 const Result = () => {
     return (
         <>
-
+            <img id="result-img1" src="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/we4l7ch6iwc"
+                alt="result-img" />
         </>
     );
 }
